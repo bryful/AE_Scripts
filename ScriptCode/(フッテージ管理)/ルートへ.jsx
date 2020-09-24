15391400902eff6a5d08758dfd9bd560558d575e
bryful <bryful@gmail.com>
2020-09-24 21:19:22 +0900

    AE_Menu

---
+++ ScriptCode/(フッテージ管理)/ルートへ.jsx
@@ -0,0 +1,26 @@
+(function(me){
+//#include "public.jsxinc"
+    
+    //
+    var scriptName = "選択したアイテムをルートに移動";
+    var toRoot = function()
+	{
+        try{
+            var selectedItems = app.project.selection;
+            if ( (selectedItems!=null)&&(selectedItems.length>0) ) {
+                app.beginUndoGroup(scriptName);
+                for (var i = 0; i < selectedItems.length; i++) {
+                    if ( selectedItems[i]  != null) {
+                        selectedItems[i].parentFolder = app.project.rootFolder;
+                    }
+                }
+                app.endUndoGroup();
+            }else{
+                alert("アイテムを選択してください。");
+            }
+        }catch(e){
+            alert("error : "+ e.toStrinh());
+        }
+	}
+    toRoot();
+})(this);
---
+++ ScriptCode/(フッテージ管理)/一つ上へ移動.jsx
@@ -0,0 +1,26 @@
+(function(me){
+//#include "public.jsxinc"
+    
+    var scriptName ="選択したアイテムを一つ上へ移動";
+    var toParent = function()
+	{
+        try{
+            var selectedItems = app.project.selection;
+            if ( (selectedItems!=null)&&(selectedItems.length>0) ) {
+                app.beginUndoGroup(scriptName);
+                for (var i = 0; i < selectedItems.length; i++) {
+                    if ( selectedItems[i]  != null) {
+                        selectedItems[i].parentFolder = selectedItems[i].parentFolder.parentFolder;
+                    }
+                }
+                app.endUndoGroup();
+            }else{
+                alert("アイテムを選択してください。");
+            }
+        }catch(e){
+             alert("error : "+ e.toStrinh());
+        }
+	}
+    toParent();
+})(this);
+ 
